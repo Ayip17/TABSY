51a70cd685723d4171a1aef83554e6abbde06cd4
Ayip17 <arifarrafi17@gmail.com>
2024-12-04 17:34:35 +0700

    Masih Error

--- app/src/main/java/com/kel3/tabsy/FavoritFragment.java
+++ app/src/main/java/com/kel3/tabsy/FavoritFragment.java
@@ -1,4 +1,18 @@
 package com.kel3.tabsy;
 
-public class FavoritFragment {
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavoritFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorit, container, false);
+    }
 }
--- app/src/main/java/com/kel3/tabsy/MainActivity.java
+++ app/src/main/java/com/kel3/tabsy/MainActivity.java
@@ -1,12 +1,17 @@
 package com.kel3.tabsy;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.kel3.tabsy.ReservasiFragment;
+import com.kel3.tabsy.FavoritFragment;
+import com.kel3.tabsy.BerandaFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +20,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Load BerandaFragment saat aplikasi dijalankan
-        Fragment berandaFragment = new BerandaFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, berandaFragment);
-        fragmentTransaction.commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        // Load default fragment (BerandaFragment)
+        loadFragment(new BerandaFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_beranda:
+                    selectedFragment = new BerandaFragment();
+                    break;
+                case R.id.nav_reservasi:
+                    selectedFragment = new ReservasiFragment();
+                    break;
+                case R.id.nav_favorit:
+                    selectedFragment = new FavoritFragment();
+                    break;
+            }
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+            }
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
